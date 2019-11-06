542889632be75a358b3599fdc3ab4da40db72b18
Eric Wang <ericwang27759@gmail.com>
2019-11-06 14:36:28 -0500

    Organizing server

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+    {
+        id: 1,
+        title: "title",
+        url: "https://courses.thinkful.com/node-postgres-v1/checkpoint/10",
+        description: "description",
+        rating: 4
+    }
+];
+
+module.exports = bookmarks;
